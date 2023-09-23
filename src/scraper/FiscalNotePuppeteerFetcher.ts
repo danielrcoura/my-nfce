374b05c430bbf99d43b0c0e729f78b6ceac7e007
satoshi <satoshi@pop-os.localdomain>
2023-09-23 10:17:00 -0300

    feat: verify captcha

--- src/scraper/FiscalNotePuppeteerFetcher.ts
+++ src/scraper/FiscalNotePuppeteerFetcher.ts
@@ -1,5 +1,5 @@
 import FiscalNoteFetcher from './FiscalNoteFetcher'
-import puppeteer from 'puppeteer';
+import puppeteer, { Page } from 'puppeteer';
 import { createWorker } from 'tesseract.js';
 
 export default class FiscalNotePupepeteerFetcher implements FiscalNoteFetcher {
@@ -7,12 +7,12 @@ export default class FiscalNotePupepeteerFetcher implements FiscalNoteFetcher {
         throw new Error('not implemented')
     }
 
-    private async fetchCaptchaImage(cookies: any[]): Promise<Buffer> {
+    private async fetchCaptchaImage(cookies: string): Promise<Buffer> {
         const response = await fetch("http://nfce.set.rn.gov.br/portalDFE/JpegImage.aspx", {
             "headers": {
                 "accept": "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
                 "accept-language": "en-US,en;q=0.9",
-                "cookie": cookies.map(cookie => `${cookie.name}=${cookie.value}`).join('; '),
+                "cookie": cookies,
                 "Referer": "http://nfce.set.rn.gov.br/portalDFE/NFCe/mDadosNFCeV2.aspx?chNFCe=24230807110635000271651060000777111415276276&Token=F39FA0A3E5B0AFA8AA6BC7457FD356CF",
                 "Referrer-Policy": "strict-origin-when-cross-origin"
             },
@@ -33,14 +33,26 @@ export default class FiscalNotePupepeteerFetcher implements FiscalNoteFetcher {
         return result.data.text;
     }
 
+    private async verifyCaptcha(page: Page, captcha: string) {
+        await page.evaluate(async (captcha) => {
+            const input = document.querySelector('#txt_cod_antirobo') as HTMLInputElement
+            input!.value = captcha;
+            const button = document.querySelector('#btnVerDanfe') as HTMLButtonElement
+            button.click()
+        }, captcha)
+        await page.screenshot({ path: 'captcha-screenshot.jpg' })
+    }
+
     private async breakCaptcha(url: string): Promise<string> {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
         await page.goto(url);
         const cookies = await page.cookies()
-        const image = await this.fetchCaptchaImage(cookies)
+        const parsedCookies = cookies.map(cookie => `${cookie.name}=${cookie.value}`).join('; ')
+        const image = await this.fetchCaptchaImage(parsedCookies)
         const captcha = await this.breakWithTesseract(image)
         const cleaned = captcha.replace(/\s/g, '')
+        await this.verifyCaptcha(page, cleaned)
         browser.close();
         return cleaned
     }
