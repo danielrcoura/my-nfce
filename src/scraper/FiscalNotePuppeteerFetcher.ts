df562c605fa2085008d6fe6cd5cb46ac1859b2c1
satoshi <satoshi@pop-os.localdomain>
2023-09-23 09:39:06 -0300

    feat: break captcha with tesseract

---
+++ src/scraper/FiscalNotePuppeteerFetcher.ts
@@ -0,0 +1,56 @@
+import FiscalNoteFetcher from './FiscalNoteFetcher'
+import puppeteer from 'puppeteer';
+import { createWorker } from 'tesseract.js';
+
+export default class FiscalNotePupepeteerFetcher implements FiscalNoteFetcher {
+    fetch(url: string): Promise<Buffer> {
+        throw new Error('not implemented')
+    }
+
+    private async fetchCaptchaImage(cookies: any[]): Promise<Buffer> {
+        const response = await fetch("http://nfce.set.rn.gov.br/portalDFE/JpegImage.aspx", {
+            "headers": {
+                "accept": "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
+                "accept-language": "en-US,en;q=0.9",
+                "cookie": cookies.map(cookie => `${cookie.name}=${cookie.value}`).join('; '),
+                "Referer": "http://nfce.set.rn.gov.br/portalDFE/NFCe/mDadosNFCeV2.aspx?chNFCe=24230807110635000271651060000777111415276276&Token=F39FA0A3E5B0AFA8AA6BC7457FD356CF",
+                "Referrer-Policy": "strict-origin-when-cross-origin"
+            },
+            "body": null,
+            "method": "GET"
+        });
+        const arrayBuffer = await response.arrayBuffer()
+        return Buffer.from(arrayBuffer)
+    }
+
+    private async breakWithTesseract(image: Buffer) {
+        const worker = await createWorker();
+        await worker.loadLanguage('eng');
+        await worker.initialize('eng');
+        await worker.setParameters({tessedit_char_whitelist: '0123456789'});
+        const result = await worker.recognize(image);
+        worker.terminate();
+        return result.data.text;
+    }
+
+    private async breakCaptcha(url: string): Promise<string> {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(url);
+        const cookies = await page.cookies()
+        const image = await this.fetchCaptchaImage(cookies)
+        const captcha = await this.breakWithTesseract(image)
+        const cleaned = captcha.replace(/\s/g, '')
+        browser.close();
+        return cleaned
+    }
+
+    private async fetchPage(captcha: string) {
+        console.log('my captcha:', captcha)
+    }
+
+    async init(url: string) {        
+        const captcha = await this.breakCaptcha(url)
+        return this.fetchPage(captcha)
+    }
+}
